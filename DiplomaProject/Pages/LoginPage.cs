bea2b55ca1c0e0e25e12feaa4b88c8e2e099ffea
Elena Kruk <krukelenka84@gmail.com>
2023-06-29 20:42:22 +0300

    [+] Add Login page feature

---
+++ DiplomaProject/Pages/LoginPage.cs
@@ -0,0 +1,61 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiplomaProject.Pages
+{
+    public class LoginPage : BasePage
+    {
+        private static readonly By EmailInputBy = By.Name("email");
+        private static readonly By PasswordInputBy = By.Name("password");
+        private static readonly By RememberMeCheckboxBy = By.Name("remember");
+        private static readonly By LoginButtonBy = By.XPath("//button[text()='Log in']");
+
+
+        public LoginPage(IWebDriver driver, bool openPageByURL = false) : base(driver, openPageByURL)
+        {
+        }
+
+        public override bool IsPageOpened => _waitService.GetVisibleElement(LoginButtonBy) != null;
+
+        protected override string Endpoint => "auth/login";
+
+
+        public IWebElement EmailInput
+        {
+            get
+            {
+                return _driver.FindElement(EmailInputBy);
+            }
+        }
+
+        public IWebElement PasswordInput
+        {
+            get
+            {
+                return _driver.FindElement(PasswordInputBy);
+            }
+        }
+
+        public IWebElement RememberMeCheckbox
+        {
+            get
+            {
+                return _driver.FindElement(RememberMeCheckboxBy);
+            }
+        }
+
+        public IWebElement LoginButton
+        {
+            get
+            {
+                return _driver.FindElement(LoginButtonBy);
+            }
+        }
+    }
+
+    
+}
---
+++ DiplomaProject/Tests/LoginTest.cs
@@ -0,0 +1,44 @@
+﻿using DiplomaProject.Core;
+using DiplomaProject.Pages;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiplomaProject.Tests
+{
+    public class LoginTest
+    {
+
+        protected IWebDriver _driver;
+
+        [SetUp]
+        public void SetUp() 
+        {
+            _driver = new Browser().Driver; 
+                
+        }
+
+        [Test]
+        public void SuccessfulLoginTest()
+        {
+            var loginPage = new LoginPage(_driver, true);
+
+            loginPage.EmailInput.SendKeys("krukelenka84@gmail.com");
+            loginPage.PasswordInput.SendKeys("krukelenka84");
+            loginPage.LoginButton.Click();
+
+            Thread.Sleep(3000);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _driver.Quit();
+            _driver.Dispose();
+        }
+
+    }
+}
