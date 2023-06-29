8b86881777040259318a84047c0fab21ccbe4028
Elena Kruk <krukelenka84@gmail.com>
2023-06-29 21:17:58 +0300

    [+]Add wrappers feature

---
+++ DiplomaProject/Wrappers/Input.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiplomaProject.Wrappers
+{
+    public class Input
+    {
+        private readonly IWebElement _element;
+
+        public Input(IWebElement element)
+        {
+            _element = element;
+        }
+
+        public bool Displayed => _element.Displayed;
+
+        public string Text => _element.Text;
+
+
+        public void Click()
+        {
+            _element.Click();
+        }
+
+        public void SendKeys(string text)
+        {
+            _element.SendKeys(text);
+        }
+    }
+}
+
